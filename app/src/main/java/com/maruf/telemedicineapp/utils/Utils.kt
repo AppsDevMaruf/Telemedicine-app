b9815b456acfa533a8bda60f1a9dde11b77a34cb
Maruf Alam <44240762+AppsDevMaruf@users.noreply.github.com>
2023-07-07 01:23:59 +0600

    add navbar

--- app/src/main/java/com/maruf/telemedicineapp/utils/Utils.kt
+++ app/src/main/java/com/maruf/telemedicineapp/utils/Utils.kt
@@ -6,23 +6,23 @@ object Utils {
     val sliderItems = listOf(
         SliderModel(
             id=1,
-            title = "Welcome To Dhaka",
-            slideImg = "https://images.macrumors.com/t/3SwpDI7nrMQeeIro9X7SbILE4_I=/1600x0/article-new/2021/03/Facebook-Feature.jpg"
+            title = "Welcome Our Apps",
+            slideImg = "https://images.pexels.com/photos/263402/pexels-photo-263402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         ),
         SliderModel(
             id=2,
-            title = "Welcome To Cumilla",
-            slideImg = "https://images.unsplash.com/photo-1688362379203-9cdf13041d41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
+            title = "",
+            slideImg = "https://images.pexels.com/photos/1692693/pexels-photo-1692693.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         ),
         SliderModel(
             id=3,
-            title = "Welcome To Sylet",
-            slideImg = "https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
+            title = "",
+            slideImg = "https://images.pexels.com/photos/3769151/pexels-photo-3769151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         ),
         SliderModel(
             id=4,
-            title = "Welcome To Sydney",
-            slideImg = "https://images.unsplash.com/photo-1674574124649-778f9afc0e9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
+            title = "",
+            slideImg = "https://images.pexels.com/photos/3279203/pexels-photo-3279203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         )
     )
 }
